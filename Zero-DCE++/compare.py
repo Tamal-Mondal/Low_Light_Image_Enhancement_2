ebd3ede72ef8ec50a7e0d959497d96274ef47327
kamal-iith <cs21mtech16001@iith.ac.in>
2022-04-30 12:37:06 +0530

    Compare 3 resulting images

--- Zero-DCE++/compare.py
+++ Zero-DCE++/compare.py
@@ -1,5 +1,5 @@
 """Module that generate comparable results at folder data/compare/ where first image is the: 
-    [Original Image] [Imagewithout Attention] [Image with Attention]
+    [Original Image] [Results1] [Results2]
 
     """
 
@@ -11,30 +11,36 @@ import os
 plt.rcParams.update({"figure.max_open_warning": 0})
 
 
-original = os.listdir("data/test_data/real")
-with_attention = os.listdir("data/result_Zero_DCE++/with_cbam/real")
-without_attention = os.listdir("data/result_Zero_DCE++/without_cbam/real")
-
-NUM = len(original)
+def compare_resuts(baseline_path, results1_path, results2_path):
 
 
-for num in range(NUM):
-    plt.figure(figsize=(20, 17))
+    baseline = os.listdir(baseline_path)
+    resut1 = os.listdir(results1_path)
+    results2 = os.listdir(results2_path)
 
-    img1 = imageio.imread(f"data/test_data/real/{original[num]}")
-    img2 = imageio.imread(f"data/result_Zero_DCE++/without_cbam/real/{without_attention[num]}")
-    img3 = imageio.imread(f"data/result_Zero_DCE++/with_cbam/real/{with_attention[num]}")
+    NUM = len(baseline)
 
-    plt.subplot(1, 3, 1)
-    plt.title("original")
-    plt.imshow(img1)
+    for num in range(NUM):
+        plt.figure(figsize=(20, 17))
 
-    plt.subplot(1, 3, 2)
-    plt.title("without_attention")
-    plt.imshow(img2)
+        img1 = imageio.imread(f"{baseline_path}/{baseline[num]}")
+        img2 = imageio.imread(f"{results1_path}/{resut1[num]}")
+        img3 = imageio.imread(f"{results2_path}/{results2[num]}")
 
-    plt.subplot(1, 3, 3)
-    plt.title("with_attention")
-    plt.imshow(img3)
+        plt.subplot(1, 3, 1)
+        plt.title("Baseline")
+        plt.imshow(img1)
 
-    plt.savefig(f"data/compare/compare_{num}.jpg")
+        plt.subplot(1, 3, 2)
+        plt.title("Result-1")
+        plt.imshow(img2)
+
+        plt.subplot(1, 3, 3)
+        plt.title("Result-2")
+        plt.imshow(img3)
+
+        plt.savefig(f"data/compare/compare_{num}.jpg")
+
+
+if __name__ == "__main__":
+    compare_resuts("data/test_data/real", "data/result_Zero_DCE++/baseline", "data/result_Zero_DCE++/baseline")
