4961f367365a8ef34b9cc707113756af32bba7aa
Tamal-Mondal <cs19btech11056@l1055-00.cse.iith.ac.in>
2022-04-30 09:19:21 +0530

    baseline results generated

--- Zero-DCE++/lowlight_test.py
+++ Zero-DCE++/lowlight_test.py
@@ -30,15 +30,14 @@ def lowlight(image_path):
 	data_lowlight = data_lowlight.cuda().unsqueeze(0)
 
 	DCE_net = model.enhance_net_nopool(scale_factor).cuda()
-	DCE_net.load_state_dict(torch.load('checkpoints/attention/Epoch9.pth'))
+	DCE_net.load_state_dict(torch.load('checkpoints/without_attention/Epoch9.pth'))
 	start = time.time()
 	enhanced_image,params_maps = DCE_net(data_lowlight)
-	print(data_lowlight.shape, enhanced_image.shape)
     
 	end_time = (time.time() - start)
 
 	print(end_time)
-	image_path = image_path.replace('test_data/real', '/result_Zero_DCE++/with_cbam')
+	image_path = image_path.replace('test_data/real', '/result_Zero_DCE++/baseline')
 
 	result_path = image_path
 	if not os.path.exists(image_path.replace('/'+image_path.split("/")[-1],'')):
